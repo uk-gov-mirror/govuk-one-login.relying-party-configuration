75f848e7f50e6d818df82ebae70571be8d1b6476
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-04-23 16:26:04 +0100

    TEMP: Add lambda to interact with dynamo

--- src/temp-api.ts
+++ src/temp-api.ts
@@ -1,10 +1,19 @@
+import { DynamoDBClient, ListTablesCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyResult, Handler } from "aws-lambda";
 
 export const handler: Handler = async (): Promise<APIGatewayProxyResult> => {
+  const dynamoClient = new DynamoDBClient({
+    region: "eu-west-2",
+  });
+
+  const docClient = DynamoDBDocument.from(dynamoClient);
+  const tables = await docClient.send(new ListTablesCommand());
   return Promise.resolve({
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello World",
+      message: `Hello World`,
+      tables,
     }),
   });
 };
